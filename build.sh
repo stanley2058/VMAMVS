82156479bf798ea33f316aff3fe51fb1bd7e3a2c
stanley2058 <stanley2058@yahoo.com.tw>
2021-07-06 14:07:31 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@
 
 mvn clean install -Dmaven.test.skip=true
 cp ./target/service-graph-platform-0.0.1-SNAPSHOT.jar app.jar
+
+docker stop VMAMVS || true
+docker rm VMAMVS || true
+docker build -t vmamvs-image:latest .
